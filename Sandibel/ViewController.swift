1f6daa72c74c7e9fa2b2e4c4137e59253ef8febc
Răzvan Ionescu <ionescu77@gmail.com>
2018-06-30 17:41:27 +0200

    Initial Commit

---
+++ Sandibel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sandibel
+//
+//  Created by Razvan Andrei Ionescu on 30/06/2018.
+//  Copyright © 2018 AV Proiect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
